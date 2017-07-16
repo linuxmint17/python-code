a7a5bc250b3d90dfafac6fc2ecbba6f260f6488b
suse <opensuselinux13.2@gmail.com>
2017-07-16 09:41:40 +0800

    sth. aobout main function

---
+++ pythonmainfunc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+'python main funciton knowledge'
+
+def main():
+	print "hello world"
+
+
+if __name__=="__main__":
+	main()
+
+def notmain():
+	print 'i am not main function'
