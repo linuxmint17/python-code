0b775b6d03e88773ea7b3c60660171477e7eaa55
suse <opensuselinux13.2@gmail.com>
2017-07-17 13:09:41 +0800

    the condition of leap become easier

--- leapyear.py
+++ leapyear.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python2.7
 'classic problem of leap year'
 def judgeleap(num):
-	if num%4!=0:
-		return 'non leap year'
-	elif num%100!=0 :
-		return 'leap year'
-	elif num%400!=0:
-		return 'non leap year'
+	# if num%4!=0:
+	# 	return 'non leap year'
+	# elif num%100!=0 :
+	# 	return 'leap year'
+	# elif num%400!=0:
+	# 	return 'non leap year'
+	# else:
+	# 	return 'leap year'
+	if num%400==0 or (num%4==0 and num%100!=0):
+	 return 'leap year'
 	else:
-		return 'leap year'
+		return 'non leap year'
 year=int(raw_input('enter a year'))
 result=judgeleap(year)
-print '%s is %s' %(year,result)
+# print '%s is %s' %(year,result)
