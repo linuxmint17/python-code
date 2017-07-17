970a160efa071c9e3fca61e39f31da44247fe2c7
suse <opensuselinux13.2@gmail.com>
2017-07-17 14:30:44 +0800

    another boring program

---
+++ geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.7
+'calculate area and volume '
+def areasquare(lenth):
+	area=lenth*lenth
+	return area
+def areacircle(radius):
+	area=3.14*radius*radius
+	return area
+def volcube(lenth):
+	vol=lenth**3
+	return vol
+def volsphere(radius):
+	vol=3.14*(4.0/3)*radius**3
+	return vol
+
