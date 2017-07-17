a0e8d33300cf9764660fc7f3e27d1c492628b461
suse <opensuselinux13.2@gmail.com>
2017-07-17 14:31:33 +0800

    boring program exicercise

---
+++ algebra.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+def algebra(inputstr):
+	mylist=inputstr.split()
+	num1=float(mylist[0])
+	num2=float(mylist[2])
+	if mylist[1]=='+':
+		return num1+num2
+	if mylist[1]=='-':
+		return num1-num2
+	if mylist[1]=='*':
+		return num1*num2
+	if mylist[1]=='/':
+		return num1/num2
+	if mylist[1]=='**':
+		return num1**num2
+	if mylist[1]=='%':
+		return num1%num2
+
+inputstr=raw_input("enter a experssion like ' 1 + 2'\nseparate number and the operator with blank\n")
+result=algebra(inputstr)
+print result
