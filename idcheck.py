12e1deb38778aa4ccde54b743773cbd2f3f40fc7
suse <opensuselinux13.2@gmail.com>
2017-07-18 14:57:52 +0800

    a little imporvement, change from 'alphas+nums' to 'alphanums'

--- idcheck.py
+++ idcheck.py
@@ -3,7 +3,7 @@
 import string
 alphas=string.letters+'_'
 nums=string.digits
-
+alphanums=alphas+nums
 print 'welcom to the Identifier checker ver1.0'
 print 'Testees must be at least 2 chars long.'
 myInput=raw_input('Inditifier to to test?\n')
@@ -14,8 +14,8 @@ if len(myInput)>1:
     print "invalid :first symbol must be alphabetic"
   else:
     for otherchar in myInput[1:]:
-      if otherchar not in alphas+nums:
+      if otherchar not in alphanums:
         print "invalid:reamining symbols must be alphanumer"
         break
-      else:
-        print "ok as an identifier"
+    else:
+      print "ok as an identifier"
