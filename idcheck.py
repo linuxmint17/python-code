e1e81e1873dada02debe72c3bb104842c8ab3b27
suse <opensuselinux13.2@gmail.com>
2017-07-20 09:44:10 +0800

    a little imporvement ,the keyword is forbidden as file name

--- idcheck.py
+++ idcheck.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python2.7
 'check the name is valid or not'
-import string
+import string,keyword
+def substring(str1,substr):
+	if str1.count(substr)!=0:
+		return True
+	else:
+		return False
+
 alphas=string.letters+'_'
 nums=string.digits
 alphanums=alphas+nums
+forbidden=keyword.kwlist
 print 'welcom to the Identifier checker ver1.0'
 print 'Testees must be at least 2 chars long.'
 myInput=raw_input('Inditifier to to test?\n')
@@ -17,5 +24,10 @@ if len(myInput)>1:
       if otherchar not in alphanums:
         print "invalid:reamining symbols must be alphanumer"
         break
+
+      elif  myInput in forbidden:
+      	print "invalid:should not be keyword"
+      	break
+
     else:
       print "ok as an identifier"
